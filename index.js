ed3e27a0a6c6370fd697eb3d86083d55ab045d36
Igor Czerwinski <igor.czerwinski@fundingcircle.com>
2016-01-03 19:52:25 +0000

    Buffer files before generating the docs.

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-import Buffer from 'buffer';
+import { Buffer } from 'buffer';
 import _ from 'lodash';
 import through from 'through2';
-import { PluginError, isBuffer, File } from 'gulp-util';
+import { PluginError, File } from 'gulp-util';
+import { readFileSync } from 'fs';
 
 import markupParser from './lib/markupParser';
 import renderer from './lib/renderer';
 
-export default function circus(template, groupBy) {
-
+export default function circus(templatePath, groupBy) {
+  const template = readFileSync(templatePath).toString();
   const render = renderer(template);
+  const jsonDocs = [];
 
-  return through.obj(function(chunk, encoding, next) {
-    if(! File.isVinyl(chunk) || ! isBuffer(chunk.contents)) {
-      throw new PluginError('circus', 'The given stream does not look like a vinyl stream');
+  return through({ objectMode: true },
+    function transform(chunk, encoding, next) {
+      if(chunk.isNull()) {
+        next();
+        return;
+      }
+
+      if(chunk.isStream()) {
+        throw new PluginError('circus', 'Streaming is not supported.');
+      }
+
+      jsonDocs.push(...markupParser(chunk.contents.toString()));
+      next();
+    },
+    function finish(done) {
+      const blocks = render(jsonDocs, groupBy);
+
+      _.pairs(blocks)
+        .map(function([path, contents]) {
+          return new File({
+            path:  `./${path}.html`,
+            contents: new Buffer(contents)
+          });
+        })
+        .forEach(file => this.push(file));
+
+      done();
     }
-
-    const data = markupParser(chunk.toString());
-    const blocks = render(data, groupBy);
-
-    _.pairs(blocks)
-      .map(function([contents, path]) {
-        return new File({
-          base: path.join(__dirname, './circus/'),
-          cwd: __dirname,
-          path: path.join(__dirname, `./circus/${path}.html`),
-          contents: new Buffer(contents)
-        });
-      })
-      .forEach(file => this.push(file));
-
-    next();
-  });
+  );
 }
