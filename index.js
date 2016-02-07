3f80ca881dba95e275105fc8ed7a8b1114187b1c
Igor Czerwinski <igor.czerwinski@fundingcircle.com>
2016-02-07 18:25:55 +0000

    Render templates passed into the circus function.

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ import { PluginError, File } from 'gulp-util';
 import { readFileSync } from 'fs';
 
 import markupParser from './lib/markupParser';
-import renderer from './lib/renderer';
+import renderer, { registerPartial } from './lib/renderer';
 
-export default function circus(templatePath, groupBy) {
-  const template = readFileSync(templatePath).toString();
-  const render = renderer(template);
+export default function circus({templates, groupBy}) {
+  const pageTemplate = readFileSync(templates.page, 'utf-8');
+  const indexTemplate = readFileSync(templates.index, 'utf-8');
+  const layoutTemplate = readFileSync(templates.partials[0], 'utf-8');
+
+  const pageRenderer = renderer(pageTemplate);
+  const indexRenderer = renderer(indexTemplate);
+  registerPartial('partials/layout', layoutTemplate);
+
   const jsonDocs = [];
 
   return through({ objectMode: true },
@@ -23,20 +29,36 @@ export default function circus(templatePath, groupBy) {
         throw new PluginError('circus', 'Streaming is not supported.');
       }
 
-      jsonDocs.push(...markupParser(chunk.contents.toString()));
+      try {
+        jsonDocs.push(...markupParser(chunk.contents.toString()));
+      } catch (ex) {
+        throw new PluginError(
+          'circus',
+          `Failed to parse ${chunk.path}.\nReason: ${ex.message}`
+        );
+      }
+
       next();
     },
     function finish(done) {
-      const blocks = render(jsonDocs, groupBy);
+      const docsBySection = _.groupBy(jsonDocs, groupBy);
+      const renderedPages = _.mapValues(docsBySection, pageRenderer);
+      const renderedIndex = indexRenderer(docsBySection);
+      const push = this.push.bind(this); //eslint-disable-line no-invalid-this
 
-      _.pairs(blocks)
+      _.pairs(renderedPages)
         .map(function([path, contents]) {
           return new File({
             path:  `./${path}.html`,
             contents: new Buffer(contents)
           });
         })
-        .forEach(file => this.push(file));
+        .forEach(push);
+
+      push(new File({
+        path: 'index.html',
+        contents: new Buffer(renderedIndex)
+      }));
 
       done();
     }
