1ca9120c8feac17c1157c98601c8fb2cd5ddb9dc
Igor Czerwinski <igor.czerwinski@fundingcircle.com>
2016-02-14 16:53:44 +0000

    Add transform stream to convert CSS into a YAML stream.

---
+++ test/streamTransformers/cssToYaml.spec.js
@@ -0,0 +1,68 @@
+import chai, { expect } from 'chai';
+
+import cssToYaml from '../../lib/streamTransformers/cssToYaml';
+import streamContainsHelper from '../../lib/utils/chaiStreamHelper';
+import streamFrom from '../../lib/utils/streamFrom';
+
+chai.use(streamContainsHelper);
+
+describe('cssToYaml', function() {
+
+  it('converts a simple commented yaml into a json obj', function() {
+    const fixture = `
+      /*
+       * some: value
+       */
+    `;
+
+    const output = streamFrom([fixture])
+      .pipe(cssToYaml());
+
+    return expect(output).to.be.a.stream(['some: value']);
+  });
+
+  it('converts yaml with multiple sections', function() {
+    const fixture = `
+      /*
+       * some: value
+       */
+      some other text
+      /*
+       * another: value
+       */
+    `;
+
+    const output = streamFrom([fixture])
+      .pipe(cssToYaml());
+
+    return expect(output).to.be.a.stream([
+      'some: value',
+      'another: value'
+    ]);
+  });
+
+  it('does not treat anything selectors as documentation', function() {
+    const fixture = `
+      /*
+       * some: value
+       */
+
+      * {
+        css: property;
+      }
+
+      /*
+       * another: value
+       */
+    `;
+
+    const output = streamFrom([fixture])
+      .pipe(cssToYaml());
+
+    return expect(output).to.be.a.stream([
+      'some: value',
+      'another: value'
+    ]);
+  });
+});
+
