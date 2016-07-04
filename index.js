06fb20dd3186fcfd89e74c84401985fb045cfe64
Igor Czerwinski <igor.czerwinski@fundingcircle.com>
2016-07-05 00:48:10 +0100

    Add simple error handling

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const combine = require('stream-combiner');
+const gutil = require('gulp-util');
 
-const createAggregatorStream = require('./lib/streamTransformers/aggregator');
-const createCssToYamlStream = require('./lib/streamTransformers/cssToYaml');
-const createFromVinylStream = require('./lib/streamTransformers/fromVinyl');
-const createHandlebarsStream = require('./lib/streamTransformers/handlebars');
-const createToVinylStream = require('./lib/streamTransformers/toVinyl');
-const createYamlToJsonStream = require('./lib/streamTransformers/yamlToJson');
+const aggregate = require('./lib/streamTransformers/aggregator');
+const cssToYaml = require('./lib/streamTransformers/cssToYaml');
+const fromVinyl = require('./lib/streamTransformers/fromVinyl');
+const handlebars = require('./lib/streamTransformers/handlebars');
+const toVinyl = require('./lib/streamTransformers/toVinyl');
+const yamlToJson = require('./lib/streamTransformers/yamlToJson');
 
-module.exports = function circus({templates, groupBy}) {
+module.exports = function circus({templates, groupBy, debug = false}) {
   return combine(
-    createFromVinylStream(),
-    createCssToYamlStream(),
-    createYamlToJsonStream(),
-    createAggregatorStream(groupBy),
-    createHandlebarsStream({
+    fromVinyl(),
+    cssToYaml(),
+    yamlToJson(),
+    aggregate(groupBy),
+    handlebars({
       index: templates.index,
       page: templates.page,
-      partials: templates.partials
+      partials: templates.partials,
+      debug
     }),
-    createToVinylStream()
-  );
+    toVinyl()
+  )
+  .on('error', function(error) {
+    gutil.log('circus: ', error.message);
+  });
 };
